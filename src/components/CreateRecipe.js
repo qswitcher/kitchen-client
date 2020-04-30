8bfce6bb27328ad831dede7d1df4186786e50f6a
Jeffrey Russom <jeffreyrussom@gmail.com>
2020-04-29 23:05:31 -0500

    Wire up state

--- src/components/CreateRecipe.js
+++ src/components/CreateRecipe.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Card } from './ui-toolkit';
+import { useInput } from '../hooks/input-hooks';
 
 const InputGroup = styled.div`
   margin: 16px 0;
@@ -45,33 +46,71 @@ const Submit = styled.input`
 `;
 
 const CreateRecipe = () => {
+  const { value: title, bind: bindTitle, reset: resetTitle } = useInput('');
+  const {
+    value: shortDescription,
+    bind: bindShortDescription,
+    reset: resetShortDescription,
+  } = useInput('');
+  const {
+    value: description,
+    bind: bindDescription,
+    reset: resetDescription,
+  } = useInput('');
+  const {
+    value: ingredients,
+    bind: bindIngredients,
+    reset: resetIngredients,
+  } = useInput('');
+  const {
+    value: instructions,
+    bind: bindInstructions,
+    reset: resetInstructions,
+  } = useInput('');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log({
+      title,
+      shortDescription,
+      description,
+      ingredients,
+      instructions,
+    });
+    resetTitle();
+    resetShortDescription();
+    resetDescription();
+    resetIngredients();
+    resetInstructions();
+  };
+
   return (
     <Card padding="32px" margin="32px 0">
       <h1>Add Recipe</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Row>
           <Col>
             <InputGroup>
               <label>Title</label>
-              <input />
+              <input type="text" {...bindTitle} />
             </InputGroup>
             <InputGroup>
               <label>Short Description</label>
-              <textarea />
+              <textarea {...bindShortDescription} />
             </InputGroup>
             <InputGroup>
               <label>Description</label>
-              <textarea />
+              <textarea {...bindDescription} />
             </InputGroup>
           </Col>
           <Col>
             <InputGroup>
               <label>Ingredients</label>
-              <textarea />
+              <textarea {...bindIngredients} />
             </InputGroup>
             <InputGroup>
               <label>Instructions</label>
-              <textarea />
+              <textarea {...bindInstructions} />
             </InputGroup>
           </Col>
         </Row>
